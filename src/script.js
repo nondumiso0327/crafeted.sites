a7ca43cb1b19fd06e5141cdab8d8d0ffa25b82e6
nondumiso0327 <pamellanzungu@gmail.com>
2025-08-04 11:45:01 +0200

    Updated script

--- src/script.js
+++ src/script.js
@@ -4,3 +4,12 @@ const navLinks = document.getElementById('navLinks');
 menuBtn.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
+// Close the menu if clicking outside of it
+document.addEventListener('click', (event) => {
+  const isClickInsideMenu = navLinks.contains(event.target);
+  const isClickOnButton = menuBtn.contains(event.target);
+
+  if (!isClickInsideMenu && !isClickOnButton) {
+    navLinks.classList.remove('active');
+  }
+});
